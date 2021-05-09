48dff83d0c543cd751c8b7cbd079d9127682a864
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-09 15:00:35 +0200

    whitelisted domain

--- PaymentApi/Startup.cs
+++ PaymentApi/Startup.cs
@@ -38,11 +38,14 @@ namespace PaymentApi
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection"));
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options=>options.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
