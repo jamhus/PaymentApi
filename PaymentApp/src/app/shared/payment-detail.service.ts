9cffa6a7d32dbfcd35606280cc75db3e0eae7173
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-09 15:00:55 +0200

    added post function

--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { PaymentDetail } from './payment-detail.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentDetailService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   formData: PaymentDetail = new PaymentDetail();
+  readonly baseUrl = 'http://localhost:17591/api/paymentDetails';
+
+  postPaymentDetail = () => {
+    return this.http.post(this.baseUrl, this.formData);
+  };
 }
